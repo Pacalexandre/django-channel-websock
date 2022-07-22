0891e58060a783959c7765e244c1c33eec582f63
Paulo Alexandre <pacalexandre@gmail.com>
2022-07-22 19:26:59 +0000

    contador  finalizado

--- real_time/count/consumers.py
+++ real_time/count/consumers.py
@@ -1,5 +1,14 @@
+import json
+from random import randint
+from time import sleep
+
 from channels.generic.websocket import WebsocketConsumer
 
 
 class WSConsumer(WebsocketConsumer):
-    pass
+    def connect(self):
+        self.accept()
+
+        for i in range(1000):
+            self.send(json.dumps({'text': randint(1, 100)}))
+            sleep(1)
--- real_time/real_time/asgi.py
+++ real_time/real_time/asgi.py
@@ -11,10 +11,15 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter
 from channels.routing import ProtocolTypeRouter
+
+from count.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
 })
